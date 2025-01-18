2c2b78381f7563eb9530eaa5594480858311bf86
unknown <najdenovviktor024@gmail.com>
2025-01-18 08:22:23 +0200

    test3

---
+++ a na n stepen.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+void main()
+{   int i,n,m ;
+    float a,p;
+    printf("Enter random number");
+    scanf("%f",&a);
+    i=1;
+    printf("Enter random number");
+    scanf("%d",&n);
+    if(n<0)m=-n;
+    else m=n;
+    p=1;
+    while(i<=m)
+    {
+        p=p*a;
+        i++;
+    }
+    if(n<0)
+    p=1/p;
+    printf("P=%.2f",p);
+
+
+
+
+}
